bf66734ac21cd87dd28531909c2e6eb77196f904
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-01 19:27:55 +0600

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
+
+
   return (
     <div className="App">
-
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
