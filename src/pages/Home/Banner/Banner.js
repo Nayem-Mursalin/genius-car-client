27c0c4e023f534fe019392a6a2951bac0a1f34c0
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-02 01:17:01 +0600

    66-4

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -10,8 +10,22 @@ const Banner = () => {
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} alt="" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/3">
+                    <h1 className="font-bold text-6xl text-white">
+                        Affordable <br />
+                        Price for Car <br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 right-5 top-1/2">
+                    <p className='text-white text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore!</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 right-5 top-3/4">
+                    <button className="btn btn-warning mr-5">Warning</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
