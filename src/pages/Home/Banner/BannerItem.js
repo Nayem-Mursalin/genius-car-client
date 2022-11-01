e16d3e4954d8cf6ac16dbb283f9ff3dc2c628944
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-02 01:50:09 +0600

    66-5 dynamic

---
+++ src/pages/Home/Banner/BannerItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-image'>
+                <img src={image} alt="" className="w-full" />
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/3">
+                <h1 className="font-bold text-6xl text-white">
+                    Affordable <br />
+                    Price for Car <br />
+                    Servicing
+                </h1>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 right-5 top-1/2">
+                <p className='text-white text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, hic. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolore!</p>
+            </div>
+            <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 right-5 top-3/4">
+                <button className="btn btn-warning mr-5">Warning</button>
+                <button className="btn btn-outline btn-warning">Warning</button>
+            </div>
+            <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div >
+    );
+};
+
+export default BannerItem;
