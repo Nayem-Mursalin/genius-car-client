3d4c8d5bea1bdec501c4c51ddf3cd3cfb9c8d042
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-04 11:43:05 +0600

    67-4 checkout page

--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/Authprovider';
 
 const CheckOut = () => {
-    const { title } = useLoaderData();
+    const { _id, title, price } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+    const handlePlaceOrder = (event) => {
+        event.preventdefault();
+        const form = event.target;
+        const name = `${form.firstName.value} ${form.lastName.value}`;
+        const email = user?.email || 'unregistered';
+        const phone = form.phone.value;
+        const message = form.message.value;
+
+        const order = {
+            service: _id,
+            serviceName: title,
+            price,
+            customer: name,
+            email,
+            phone,
+            message
+        }
+    }
+
     return (
-        <div>
-            <h2>CheckOut : {title}</h2>
-        </div>
+        <form onSubmit={handlePlaceOrder}>
+            <h4 className="text-4xl">You are about to Order: {title}</h4>
+            <h4 className="text-3xl">Price: {price}</h4>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                <input name='firstName' type="text" placeholder="First Name" className="input input-bordered w-full " />
+                <input name='lastName' type="text" placeholder="Last Name" className="input input-bordered w-full " />
+                <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered w-full " />
+                <input name='email' type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
+            </div>
+            <textarea name='message' className="textarea textarea-bordered h-24 w-full" placeholder="Your Message"></textarea>
+            <input className='btn btn-primary' type="submit" value="Place Your Order" />
+        </form>
     );
 };
 
